3bbeffc84b8ba2a6265b8e98cae40cf702cd2772
Andrey Mitin <amitin@iponweb.net>
2022-01-10 16:02:05 +0300

    initial commit

---
+++ utils.py
@@ -0,0 +1,45 @@
+import random
+
+from datacenter.models import Lesson, Mark, Chastisement, Commendation, Schoolkid
+from django.core.exceptions import ObjectDoesNotExist, MultipleObjectsReturned
+
+COMMENDATIONS = [
+    'Хвалю!',
+    'Молодец!',
+    'Ребенок хорошо себя проявил!',
+    'Пример для подражания!',
+    'Хорошо себя вел'
+]
+
+def get_schoolkid(schoolkid_name): 
+    try:
+        schoolkid = Schoolkid.objects.get(full_name__contains=schoolkid_name)
+    except ObjectDoesNotExist:
+        raise Exception('Ученик не найден!')
+    except MultipleObjectsReturned:
+        return Exception('Найдено несколько учеников!')
+    return schoolkid
+
+def fix_marks(schoolkid_name):
+    Mark.objects.filter(schoolkid=get_schoolkid(schoolkid_name), points__in=[2,3]).update(points=5)
+
+def remove_chastisements(schoolkid_name):
+    Chastisement.objects.filter(schoolkid=get_schoolkid(schoolkid_name)).delete()
+
+def count_bad_marks(schoolkid_name):
+    print(Mark.objects.filter(schoolkid=get_schoolkid(schoolkid_name), points__in=[2,3]).count())
+
+def create_commendation(schoolkid_name, lesson_title):
+    schoolkid = get_schoolkid(schoolkid_name)
+    last_lesson = Lesson.objects.filter(
+        year_of_study=schoolkid.year_of_study,
+        group_letter=schoolkid.group_letter,
+        subject__title=lesson_title
+    ).last()
+    Commendation.objects.create(
+        text=random.choice(COMMENDATIONS),
+        schoolkid=schoolkid,
+        created=last_lesson.date,
+        subject=last_lesson.subject,
+        teacher=last_lesson.teacher
+    )
